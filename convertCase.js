66d0faf99b6d5838d6bef325b33045c308bbf463
DESKTOP-QT869R2\syota <hurunto@gmail.com>
2025-01-14 21:53:28 +0900

    キャメルケース、スネークケース変換処理を追加

--- convertCase.js
+++ convertCase.js
@@ -57,11 +57,11 @@ class ConvertCase {
                 break;
             // キャメルケース
             case CONVERT_TO_CAMEL_CASE:
-                alert("キャメルケース");
+                this.ConvertToCamelCase();
                 break;
             // スネークケース
             case CONVERT_TO_SNAKE_CASE:
-                alert("スネークケース");
+                this.ConvertToSnakeCase();
                 break;
             // それ以外
             default:
@@ -88,7 +88,7 @@ class ConvertCase {
         for (let i = 0; i < elements.length; i++) {
             if (elements[i].checked) {
             checkValue = elements[i].value;
-            break; // 見つかった時点でループ終了
+            break; 
             }
         }
 
@@ -137,39 +137,70 @@ class ConvertCase {
         }
     }
 
-    // // *********************************
-    // // メソッド：ConvertToLower
-    // // 処理内容：大文字→小文字変換処理
-    // // 引数　　：なし
-    // // 戻り値　：なし
-    // // *********************************
-    // ConvertToLower(){
-    //     try{
-    //         // 
-    //     }
-    //     catch(e){
-    //         alert(e.message);
-    //     }
-    // }
+    // *********************************
+    // メソッド：ConvertToCamelCase
+    // 処理内容：キャメル式変換処理
+    // 引数　　：なし
+    // 戻り値　：なし
+    // *********************************
+    ConvertToCamelCase(){
+        try{
+            // スペース、「_」、「-」で文字列を区切る
+            var tagetStr = this.inputTextBox.value;
+            var separatorStr = /[\s_-]+/;
+            var arrayStr = tagetStr.split(separatorStr);
+            
+            // キャメルケースに変換する
+            let outputStr = ""
+            for(let i=0;i<arrayStr.length;i++){
+                let workStr = arrayStr[i];
+                let firstChar = ""
+                // 最初の要素の1文字目はそのまま
+                if(i === 0){
+                    firstChar = workStr.substr(0,1).toLowerCase();
+                    outputStr += workStr;
+                    continue;
+                }
 
-    // // *********************************
-    // // メソッド：ConvertToLower
-    // // 処理内容：大文字→小文字変換処理
-    // // 引数　　：なし
-    // // 戻り値　：なし
-    // // *********************************
-    // ConvertToLower(){
-    //     try{
-    //         // 
-    //     }
-    //     catch(e){
-    //         alert(e.message);
-    //     }
-    // }
+                // 以降の要素は文字列の1文字目を大文字に変換する
+                firstChar = workStr.substr(0,1).toUpperCase();
+                let tailStr = workStr.substr(1);
+                outputStr += firstChar + tailStr;
+            }
+
+            // 出力テキストボックスに出力
+            this.outputTextBox.value = outputStr;
+        }
+        catch(e){
+            alert(e.message);
+        }
+    }
+
+    // *********************************
+    // メソッド：ConvertToSnakeCase
+    // 処理内容：スネークケース変換処理
+    // 引数　　：なし
+    // 戻り値　：なし
+    // *********************************
+    ConvertToSnakeCase(){
+        try{
+            // スペース、「_」、「-」で文字列を区切り、小文字に変換
+            let targetStr = this.inputTextBox.value.toLowerCase();
+            let separatorStr = /[\s_-]+/;
+            let arrayStr = targetStr.split(separatorStr);
+            
+            // スネークケースに変換する
+            let outputStr = arrayStr.join("_");
+
+            // 出力テキストボックスに出力
+            this.outputTextBox.value = outputStr;
+        }
+        catch(e){
+            alert(e.message);
+        }
+    }
   }
 
-    
-  
   // クラスのインスタンスを作成してイベントリスナーを有効化
   new ConvertCase();
   
